8dd43a8db6809cef508c5a08bd7e1c1922e83891
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-03-12 10:49:52 +0300

    Add files via upload

---
+++ Analytics/index.js
@@ -0,0 +1,38 @@
+"use script"
+
+// let altr = setTimeout (function() {
+//   alert("waiting")
+// }2000)
+
+
+
+// let altr = setTimeout (function(a ,b) {
+//   console.log(a+b);
+// },2000,
+// 20,
+// 30)
+
+
+// let settimer =setTimeout(
+//   (a ,b) =>{
+//     console.log(a+b);
+//   },1000,
+//   12,
+//   34)
+
+// if(13>20){
+//   clearTimeout(settimer)
+// }
+
+// let m = 0;
+// let setint = setInterval(
+//   function(a,b){
+//     console.log(a+b, m);
+//     m++;
+
+//     if(m==10){
+//       clearInterval(setint)
+//     }
+//   },1000,
+//   12,
+//   34)
